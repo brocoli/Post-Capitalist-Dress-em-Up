2c360ac3b6fa3d2518a43744072cf222472f939b
@virgulaleal <aqueleleal@gmail.com>
2019-04-05 23:31:03 -0300

    Character animation, camera

---
+++ components/AnimationController.gd
@@ -0,0 +1,44 @@
+extends Node
+
+var velocity: Vector2
+var directions: Array
+var last_directions: Array = ["right"]
+
+## would be nice:
+# enum animation_states {IDLE, WALK, RUN, CHANNEL}
+
+func _physics_process(_delta: float) -> void:
+	velocity = get_parent().get_node("MovementController").velocity
+
+	if directions.size() > 0:
+		last_directions = directions
+
+	directions.clear()
+
+	if velocity.x == 0 and velocity.y == 0:
+		if not "idle" in directions:
+			directions.append("idle")
+
+	if velocity.x > 0:
+		if not "right" in directions:
+			directions.append("right")
+	elif velocity.x < 0:
+		if not "left" in directions:
+			directions.append("left")
+
+	if velocity.y > 0:
+		if not "down" in directions:
+			directions.append("down")
+	elif velocity.y < 0:
+		if not "up" in directions:
+			directions.append("up")
+
+	if "idle" in directions:
+		if "right" in last_directions:
+			get_parent().get_node("AnimatedSprite").animation = "idle_right"
+		elif "left" in last_directions:
+			get_parent().get_node("AnimatedSprite").animation = "idle_left"
+	elif "right" in directions:
+		get_parent().get_node("AnimatedSprite").animation = "walk_right"
+	elif "left" in directions:
+		get_parent().get_node("AnimatedSprite").animation = "walk_left"
--- components/movement-controller.gd
+++ components/movement-controller.gd
@@ -1,21 +1,21 @@
 extends Node
 
-export (int) var speed = 200
+export (int) var speed: int = 200
 
-var velocity = Vector2()
+var velocity: Vector2 = Vector2()
 
-func get_input():
-    velocity = Vector2()
-    if Input.is_action_pressed('move_right'):
-        velocity.x += 1
-    if Input.is_action_pressed('move_left'):
-        velocity.x -= 1
-    if Input.is_action_pressed('move_down'):
-        velocity.y += 1
-    if Input.is_action_pressed('move_up'):
-        velocity.y -= 1
-    velocity = velocity.normalized() * speed
+func get_input() -> void:
+	velocity = Vector2()
+	if Input.is_action_pressed('move_right'):
+		velocity.x += 1
+	if Input.is_action_pressed('move_left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('move_down'):
+		velocity.y += 1
+	if Input.is_action_pressed('move_up'):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
 
-func _physics_process(_delta):
-    get_input()
-    get_parent().move_and_slide(velocity)
+func _physics_process(_delta: float) -> void:
+	get_input()
+	get_parent().move_and_slide(velocity)
